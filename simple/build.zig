9b3a47912c983f2044aab1ae8d8606aef50f583e
Tobias Simetsreiter <dasimmet@gmail.com>
2025-01-11 21:49:13 +0100

    also update simple example

--- simple/build.zig
+++ simple/build.zig
@@ -4,22 +4,21 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const dir = std.Build.InstallDir.bin;
 
-    const zjb = b.dependency("zjb", .{});
-
     const simple = b.addExecutable(.{
         .name = "simple",
         .root_source_file = b.path("src/simple.zig"),
         .target = b.resolveTargetQuery(.{ .cpu_arch = .wasm32, .os_tag = .freestanding }),
         .optimize = optimize,
     });
-    simple.root_module.addImport("zjb", zjb.module("zjb"));
     simple.entry = .disabled;
     simple.rdynamic = true;
 
-    const extract_simple = b.addRunArtifact(zjb.artifact("generate_js"));
-    const extract_simple_out = extract_simple.addOutputFileArg("zjb_extract.js");
-    extract_simple.addArg("Zjb"); // Name of js class.
-    extract_simple.addArtifactArg(simple);
+    const zjb = b.dependency("zjb", .{
+        .@"wasm-bindgen-bin" = simple.getEmittedBin(),
+    });
+    const extract_simple_out = zjb.namedLazyPath("zjb_extract.js");
+
+    simple.root_module.addImport("zjb", zjb.module("zjb"));
 
     const simple_step = b.step("simple", "Build the hello Zig example");
     simple_step.dependOn(&b.addInstallArtifact(simple, .{
